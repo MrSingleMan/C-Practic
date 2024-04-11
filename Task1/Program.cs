172e67a67cfe696eb5532d0020111d2f0713b082
Alexander <n00mer00k@gmail.com>
2024-04-11 22:46:13 +0400

    Task1 multipe

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine($"{num}, multiple of 7 and 23");
+    }
+       else System.Console.WriteLine($"{num}, not multiple of 7 and 23");
